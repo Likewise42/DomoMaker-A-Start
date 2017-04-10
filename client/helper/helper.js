88264e0111254a8b3144f3a3084e66226f31bd0a
Ryan Muskopf (RIT Student) <rjm9187@ad.rit.edu>
2017-04-10 15:10:52 -0400

    d done

---
+++ client/helper/helper.js
@@ -0,0 +1,29 @@
+const handleError = (message) =>{
+	$('#errorMessage').text(message);
+	$('#domoMessage').animate({width:'toggle'},350);
+};
+
+const redirect = (response) =>{
+	$('#domoMessage').animate({width:'hide'},350);
+	window.location = response.redirect;
+}
+
+const sendAjax = (type, action, data, success) =>{
+	console.dir(type);
+	console.dir(action);
+	console.dir(data);
+	console.dir(success);
+	$.ajax({
+		cache: false,
+		type: type,
+		url: action,
+		data: data,
+		dataType: "json",
+		success: success,
+		error: function(xhr, status, error) {
+			console.log(xhr.responseText);
+			var messageObj = JSON.parse(xhr.responseText);
+			handleError(messageObj.error);
+		}
+	});
+};
